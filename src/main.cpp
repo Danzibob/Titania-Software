7637567daa2b51aa1b23a993566546a9c9c7ddb9
Danny Roberts <danzibob@gmail.com>
2024-05-31 16:43:32 +0100

    emf

--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,113 @@
-#include <Arduino.h>
 #include <Adafruit_BMP280.h>
+#include <Arduino.h>
 #include <EEPROM.h>
-#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-
+#include <ESP8266WiFi.h>
 
 #define LED_PIN 1
-#define TOGGLE  3
+#define TOGGLE 3
 
-#define BUFF_SIZE 4000
+#define LOCAL_ENDPOINT "http://172.23.0.130:8080/test"
+#define CLOUD_ENDPOINT "http://88.208.197.140:8080/test"
 
-#define ENDPOINT "http://172.23.0.130:8080/test"
+#define BUFF_SIZE 6000
+#define MS_PER_READ 200
 
-const char* ssid = "Danzi'sPixel";      // WiFi SSID
-const char* password = "RocketGoWoosh"; // Password
+float c_buff[BUFF_SIZE * 2];
+uint buff_ptr = 0;
+ulong next_log_time = 0;
+
+const char* ssid = "Danzi'sPixel";       // WiFi SSID
+const char* password = "RocketGoWoosh";  // Password
 
 Adafruit_BMP280 bmp;
+float baseline_pressure;
 
-int toggle_val;
-int ok = 0;
-float max_alt = 0.0;
-float base_pressure = 0.0;
-
-// Circular buffer to hold recent altitude data
-uint8_t buffer_ptr = 0;
-float c_buff[BUFF_SIZE*2];
-
-
-void blinkN(int N){
-  for(; N > 0; N--){
-    digitalWrite(LED_PIN, LOW);
-    delay(50);
-    digitalWrite(LED_PIN, HIGH);
-    delay(200);
-  }
+void blinkN(int N) {
+    for (; N > 0; N--) {
+        digitalWrite(LED_PIN, LOW);
+        delay(50);
+        digitalWrite(LED_PIN, HIGH);
+        delay(200);
+    }
 }
 
-void blinkVal(int val){
-    int hundreds = (int)(val / 100) % 10;
-    int tens = (int)(val / 10)%10;
-    int ones = (int)(val)%10;
-    delay(1000);
-    blinkN(hundreds);
-    delay(500);
-    blinkN(tens);
-    delay(500);
-    blinkN(ones);
-}
-
-void getTime(){
-
+void blinkVal(uint val) {
+    for (uint place = 1; place < val; place *= 10) {
+        delay(600);
+        blinkN((val / place) % 10);
+    }
 }
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(TOGGLE, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(TOGGLE, INPUT);
 
-  WiFi.disconnect();  // Ensure we're not trying to wifi yet
+    WiFi.disconnect();  // Ensure we're not trying to wifi yet
 
-  delay(2500);
+    delay(2500);
 
-  Wire.begin (0,2);
-  ok = bmp.begin(0x76);
-  base_pressure = bmp.readPressure()/100.0;
-
-  EEPROM.begin(16);
-  digitalWrite(LED_PIN, HIGH);
+    Wire.begin(0, 2);
+    while (!bmp.begin(0x76)) {
+        blinkVal(313);
+        delay(2000);
+    }
+    baseline_pressure = bmp.readPressure() / 100.0;
+    digitalWrite(LED_PIN, HIGH);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  if(ok){
-    if(digitalRead(3)){
-      float altitude = bmp.readAltitude(base_pressure);
-      c_buff[buffer_ptr++] = altitude;
-      if(buffer_ptr >= BUFF_SIZE) buffer_ptr = 0;
-      delay(250);
-    } else {
-      if (WiFi.status() == WL_DISCONNECTED) WiFi.begin(ssid, password);
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(250);
-        blinkN(2);
-      }
-      int lastByte = WiFi.localIP()[3];
-      blinkVal(lastByte);
-
-      // Send data via HTTP request
-      HTTPClient http;
-      WiFiClient wifiClient;
-      http.begin(wifiClient,ENDPOINT);
-      http.addHeader("Content-Type", "application/octet-stream");
-      // First, copy first part of rung buffer to after itself
-      for(int i=0; i<buffer_ptr; i++){
-        c_buff[i+BUFF_SIZE] = c_buff[i];
-      }
-      // Then identify the starting point for the buffer
-      byte* body = (byte*)c_buff + (u32)buffer_ptr*sizeof(float);
-      // and relay BUFF_SIZE floats from that point
-      int responseCode = http.POST(body, BUFF_SIZE*sizeof(float));
-      if(responseCode > 0){
-        blinkN(responseCode/100);
-      } else {
-        blinkN(10);
-      }
+    float altitude;
+    ulong time;
+    switch (digitalRead(TOGGLE)) {
+        case 1:  // Logging
+            // Circular buffer
+            buff_ptr = (buff_ptr + 1) % BUFF_SIZE;
+            // Wait for next logging time
+            time = millis();
+            while (time < next_log_time) {
+                delay(1);
+                time = millis();
+            }
+            altitude = bmp.readAltitude(baseline_pressure);
+            next_log_time = time + MS_PER_READ;
+            c_buff[buff_ptr] = altitude;
+            break;
+        case 0:  // Exporting
+            // Connect to hotspot
+            if (WiFi.status() == WL_DISCONNECTED) WiFi.begin(ssid, password);
+            while (WiFi.status() != WL_CONNECTED) {
+                delay(400);
+                blinkN(2);
+            }
+            // Send data via HTTP request
+            HTTPClient http;
+            WiFiClient wifiClient;
+            // First, copy first part of ring buffer to after itself
+            for (uint i = 0; i < buff_ptr; i++) {
+                c_buff[i + BUFF_SIZE] = c_buff[i];
+            }
+            // Then identify the starting point for the buffer
+            uint8_t* body = reinterpret_cast<uint8_t*>(c_buff) + static_cast<uint32_t>(buff_ptr) * sizeof(float);
+            // and relay BUFF_SIZE floats from that point
+            int responseCode = -1;
+            while (1) {
+                http.begin(wifiClient, CLOUD_ENDPOINT);
+                http.addHeader("Content-Type", "application/octet-stream");
+                responseCode = http.POST(body, BUFF_SIZE * sizeof(float));
+                delay(1000);
+                if (responseCode > 0) {
+                    blinkN(responseCode / 100);
+                } else {
+                    blinkN(1);
+                }
+                // Check if the data was successfully recieved
+                if ((responseCode / 100) == 2) {
+                    while (1) {
+                        delay(1000);
+                        blinkN(2);
+                    }
+                }
+            }
     }
-  }
 }
--- web-viewer/index.js
+++ web-viewer/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+const SAVE_DIR = "./public/saves"
+
 
 const URL =  "http://172.23.0.173:5501/"
 
@@ -10,10 +12,37 @@ app.use(express.raw());
 app.use(express.static('public'));
 
 app.get('/savefiles',(_, res) => {
-    fs.readdir("./public/saves", (err, files) => {
+    fs.readdir(SAVE_DIR, (err, files) => {
         if (err) res.sendStatus(500)
         else res.json([...files]);
     })
+})
+
+app.get('/lastflight', (_, res) => {
+    fs.readdir(SAVE_DIR, (err, files) => {
+        if (err) res.sendStatus(500)
+
+        // Sort files by modification time
+        files.sort((a, b) => {
+            return fs.statSync(path.join(SAVE_DIR, b)).mtime.getTime() -
+                    fs.statSync(path.join(SAVE_DIR, a)).mtime.getTime();
+        });
+
+        // Return the most recent file
+        fs.readFile(path.join(SAVE_DIR, files[0]), 'utf8', (err, data) => {
+            if (err) res.sendStatus(500)
+
+            try {
+                let flight_data = JSON.parse(data);
+                res.json({
+                    max_altitude: Math.max(...flight_data.altitude)
+                })
+            } catch (error) {
+                res.sendStatus(500)
+            }
+        });
+    });
+
 })
 
 app.post('/test', (req, res) => {
